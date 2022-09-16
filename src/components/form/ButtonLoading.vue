28f50dd64129efc18c98cf605a9a43b5112c54ee
toussaint2v <toussaint.carlotti@gmail.com>
2022-09-16 16:12:31 +0200

    initial commit

---
+++ src/components/form/ButtonLoading.vue
@@ -0,0 +1,12 @@
+<template >
+    <button class="bt-simple bg-blue-600 hover:bg-blue-900 text-white w-32 flex justify-center h-10" type="submit">
+        <div v-if="loading"><img src="../../assets/loading.gif" width="20" alt="loading"></div>
+        <div v-else>{{ value }} </div>
+    </button>
+</template>
+
+<script>
+export default {
+    props:['loading', 'value']
+}
+</script>
